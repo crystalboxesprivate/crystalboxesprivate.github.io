a0c913eaa89cbea2799d71b8a3f8aae6917c1b74
crystalboxes <ursus95@gmail.com>
2020-08-13 00:07:32 +0300

    build

---
+++ precache-manifest.495d5cf2e40f9a8a69cedde5e55b2396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad244708ab940e0e66e75b55fce0221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a9503c8eec7dd6ea59",
+    "url": "/static/css/2.818a2f7f.chunk.css"
+  },
+  {
+    "revision": "fe110db9edb5ac78ccc0",
+    "url": "/static/css/main.c6353d74.chunk.css"
+  },
+  {
+    "revision": "c6a9503c8eec7dd6ea59",
+    "url": "/static/js/2.1a405bc6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a405bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe110db9edb5ac78ccc0",
+    "url": "/static/js/main.bae1fef7.chunk.js"
+  },
+  {
+    "revision": "3a85876f5d523b620a0f",
+    "url": "/static/js/runtime-main.9082e2fc.js"
+  }
+]);
